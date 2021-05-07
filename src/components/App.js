f6e301992b8ada5ce97dfdc606972d74d805ff73
nishant <nishantkodambal@gmail.com>
2021-05-08 03:05:13 +0530

    Newtons's school third solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    // <head data-ns-test="project-name">e-commerce</head>
+    <div id="main">
+      <h1 data-ns-test="project-name">First project</h1>
+      <div data-ns-test="project-name">e-commerce</div>
+      <div data-ns-test="project-description">project build using Html, css, javscript and react</div>
+    </div>
   )
 }
 
 
 export default App;
+
